49a90294f5e4b22dcb797b3553636b6a0aaa699e
Jimothy <timothynewton1998@gmail.com>
2021-11-05 21:51:23 +0000

    Removed redundant json dump, added debug

--- weather.py
+++ weather.py
@@ -19,17 +19,15 @@ def index():
 
 @app.route("/result", methods=["POST", "GET"])
 def result():
-    with open('forecast.json') as f:
-        forecastdict = json.load(f)                          #loads forecast json that was made in apiparsing.py
     weather_day = []
     temp = []
     detailforcst = []
     icon = []                                                #storage for individual asects of the forecast 
-    for day in forecastdict:
+    for day in parsing.fore:
         weather_day.append(day)
-        temp.append(forecastdict[day][0])
-        detailforcst.append(forecastdict[day][6])
-        icon.append(forecastdict[day][4])                     #appends each particular forecast aspect to storage for each time of day/day
+        temp.append(parsing.fore[day][0])
+        detailforcst.append(parsing.fore[day][6])
+        icon.append(parsing.fore[day][4])                     #appends each particular forecast aspect to storage for each time of day/day
     return render_template('results.html', days = weather_day, temp = temp, forecast = detailforcst, icon = icon)         #renders page and storage variables(for later access in the results.html)
 
 
